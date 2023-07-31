3db8a6295fd56e785d278b3686ca41c3f59b8791
JanCarlo03 <al221910462@gmail.com>
2023-07-31 12:35:40 -0600

    metodos LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use Laravel\Socialite\Facades\Socialite;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-//
+public function logeo (){
+    return Socialite::driver('microsoft')->redirect();
+}
+
+public function callback (){
+    dd('accediste');
+}
+
 }
