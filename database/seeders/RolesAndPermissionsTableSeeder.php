4e4dc4ecd6df91bddccd2053b7fa1c46726d2055
AlejandroPoceros <alejandropocerosreyes@gmail.com>
2023-08-01 10:29:40 -0600

    cracion de roles y permisos

--- database/seeders/RolesAndPermissionsTableSeeder.php
+++ database/seeders/RolesAndPermissionsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsTableSeeder extends Seeder
 {
@@ -12,6 +14,26 @@ class RolesAndPermissionsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //reset cached roles and permission
+        app()['cache']-> forget('maklad.permission.cache');
+    
+        //create permission
+        Permission::firstOrCreate(['name'=>'usuarios.index']);
+        Permission::firstOrCreate(['name'=>'usuarios.create']);
+        Permission::firstOrCreate(['name'=>'usuarios.store']);
+        Permission::firstOrCreate(['name'=>'usuarios.show']);
+        Permission::firstOrCreate(['name'=>'usuarios.edit']);
+        Permission::firstOrCreate(['name'=>'usuarios.update']);
+
+
+        //create roles and assign existing permissions
+        $role= Role::firstOrcreate(['name'=>'Administrador']);
+        $role-> givePermissionTo(Permission::all());
+
+
+        $role= Role::firstOrcreate(['name'=>'Staff']);
+        $role-> givePermissionTo('usuarios.index');
+
+    
     }
 }
