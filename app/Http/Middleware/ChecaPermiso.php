81da32ed313c5bea634ad4f284b442e43542fb8f
AlejandroPoceros <alejandropocerosreyes@gmail.com>
2023-08-01 12:22:31 -0600

    checa permiso

--- app/Http/Middleware/ChecaPermiso.php
+++ app/Http/Middleware/ChecaPermiso.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Exception\UnauthorizedException;
 
 class ChecaPermiso {
+/**
+ * 
+ * @param \Illuminate\Http\Request $request
+ * @param \Closure $next
+ * @return mixed
+ */
+
     public function handle ($request, Closure $next ){
 if(app('auth')->guest()){
     throw new \Spatie\Permission\Exceptions\UnauthorizedException(401,'inicio requerido');
