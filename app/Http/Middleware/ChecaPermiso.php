6217a426f35eedcb65f2ac0fcc9cd689eee97565
AlejandroPoceros <alejandropocerosreyes@gmail.com>
2023-08-01 12:18:03 -0600

    middleware checapermiso

---
+++ app/Http/Middleware/ChecaPermiso.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Exception\UnauthorizedException;
+
+class ChecaPermiso {
+    public function handle ($request, Closure $next ){
+if(app('auth')->guest()){
+    throw new \Spatie\Permission\Exceptions\UnauthorizedException(401,'inicio requerido');
+
+}
+$id =$request-> route()->getAction()['as'];
+    if(app('auth')->user()->can($id)){
+return $next($request);
+    }
+    throw new \Spatie\Permission\Exceptions\UnauthorizedException(403,'SIN PERMISO');
+}
+}
