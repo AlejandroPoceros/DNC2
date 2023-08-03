3d4778fa1961285db24d4b5586d104333c4df612
AlejandroPoceros <alejandropocerosreyes@gmail.com>
2023-08-03 10:19:18 -0600

    instalacion de bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+/* resources/js/bootstrap.js */
+import 'bootstrap';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
