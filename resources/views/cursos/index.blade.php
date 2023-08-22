f759ca74f21698b822766cd9e1ccb2582925df4d
AlejandroPoceros <alejandropocerosreyes@gmail.com>
2023-08-22 13:55:21 -0600

    modal arreglado 1.1

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -155,7 +155,7 @@
                                     <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title" >DETALLE: {{$curso->CursoNombre}} </h3>
                                     <hr/><hr/>
                                     <div class="mt-2  overflow-y-auto" >
-                                      <p class="text-sm text-gray-800"> <b style="font: italic">Descripción:</b> {{$curso->CursoDescr}} </p>
+                                      <p class="text-sm text-gray-800"> <b style="font: italic">Descripción de:</b> {{$curso->CursoDescr}} </p>
                  
 
 
@@ -166,7 +166,7 @@
                               <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                 <button id="cerrarModal" type="button" class=" inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Cerrar</button>
                                 <button id="cerrarModal" type="button" class=" inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-teal-300 sm:ml-3 sm:w-auto">Guardar</button>
-                                {{-- <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button> --}}
+                                {{-- <button      type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button> --}}
                               </div>
                             </div>
                           </div>
